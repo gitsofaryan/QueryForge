7627827ad5ba5891ec56b070b8a1bcb97c62df29
Aryan Jain <aryan.jain.csbs22@ggits.net>
2025-03-27 20:23:52 +0530

    prod: pagespeed

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,27 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import chunkSplitPlugin from 'vite-plugin-chunk-split';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    chunkSplitPlugin({
-      strategy: 'default',
-      customSplitting: {
-        'vendor': [/node_modules/],
-        'codemirror': [/codemirror/],
-        'mui': [/mui/],
-        'lodash': [/lodash/],
-      },
-    }),
-  ],
+  plugins: [vue()],
   build: {
-    minify: 'esbuild',
+    minify: 'esbuild', // Ensure minification
     terserOptions: {
       compress: {
-        drop_console: true,
+        drop_console: true, // Remove console.logs in production
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libraries into separate chunks
+          vue: ['vue'],
+          codemirror: ['vue-codemirror', 'codemirror'],
+          lodash: ['lodash'],
+        },
       },
     },
   },
   server: {
-    compress: true,
+    compress: true, // Enable gzip compression
   },
 });
