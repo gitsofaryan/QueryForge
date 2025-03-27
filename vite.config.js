0269cf8dbf49bf656b819f9cd72a52c645946732
Aryan Jain <aryan.jain.csbs22@ggits.net>
2025-03-27 20:24:59 +0530

    prod: pagespeed

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    minify: 'esbuild', // Ensure minification
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console.logs in production
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Split vendor libraries into separate chunks
-          vue: ['vue'],
-          codemirror: ['vue-codemirror', 'codemirror'],
-          lodash: ['lodash'],
-        },
-      },
+  optimizeDeps: {
+    include: ['@mui/icons-material', '@mui/material'],
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
-  server: {
-    compress: true, // Enable gzip compression
-  },
-});
+})
